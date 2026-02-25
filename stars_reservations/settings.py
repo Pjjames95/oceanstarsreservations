76164b586f593d0cae8e427c7d896fff8fc31da4
Pjjames95 <gachombajames7@gmail.com>
2026-02-25 17:41:36 +0300

    controlled reservations for the Ocean Star Restaurant

--- stars_reservations/settings.py
+++ stars_reservations/settings.py
@@ -138,12 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
