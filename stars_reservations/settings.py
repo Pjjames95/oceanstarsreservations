756c05416ba37cfce9fc1a242a7f928a03f3fb0b
Pjjames95 <gachombajames7@gmail.com>
2026-02-25 17:58:08 +0300

    controlled reservations/bookings for the Ocean Stars Restaurant

--- stars_reservations/settings.py
+++ stars_reservations/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['oceanstars.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['oceanstars.vercel.app', 'localhost', '127.0.1']
 
 
 # Application definition
